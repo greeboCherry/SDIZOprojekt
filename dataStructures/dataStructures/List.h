a9d4f2c7f7e71798583fe907fc10c0e66886ae15
greeboCherry <greebo4pl@gmail.com>
2017-03-14 14:00:34 +0100

    VS gitignore, first implementation of list with push front

---
+++ dataStructures/dataStructures/List.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <stdint.h> //int32_t
+
+class List
+{
+private:
+
+	struct Node
+	{
+	friend class List;
+	private:
+		int32_t key;	//actual value
+		Node * _next = NULL;
+		Node * _prev = NULL;
+	public:
+		Node* getPrev()
+		{
+			return _prev;
+		}
+		Node* getNext()
+		{
+			return _next;
+		}
+		int32_t getKey()
+		{
+			return key;
+		}
+		void setKey(int32_t newKey)
+		{
+			key = newKey;
+		}
+
+	};
+
+	Node * _head = NULL;	//pointer to the first node
+	Node * _tail = NULL;	//pointer to the last node
+
+public:
+	void insert(uint32_t position, int32_t value);
+
+	void pushFront(int32_t value);
+
+	void printStructure();
+
+};
+
